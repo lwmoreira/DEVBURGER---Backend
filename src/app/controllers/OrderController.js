c7d2f658ea3cb2d82bd4d4233c9b56ea6f84209b
lwmoreira <lwmoreira01@gmail.com>
2024-04-15 13:46:26 -0300

    criando controller de pedidos

---
+++ src/app/controllers/OrderController.js
@@ -0,0 +1,45 @@
+import * as Yup from 'yup'
+import Order from '../schemas/Order'
+import Product from '../models/Product'
+
+class OrderController {
+  async store(request, response) {
+    const schema = Yup.object({
+      products: Yup.array()
+        .required()
+        .of(
+          Yup.object({
+            id: Yup.number().required(),
+            quantity: Yup.number().required(),
+          }),
+        ),
+    })
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { products } = request.body
+
+    const productsIds = products.map((product) => product.id)
+
+    const findProducts = await Product.findAll({
+      where: {
+        id: productsIds,
+      },
+    })
+
+    const order = {
+      user: {
+        id: request.userId,
+        name: request.userName,
+      },
+      products: findProducts,
+    }
+
+    return response.status(201).json(order)
+  }
+}
+export default new OrderController()
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '8ea0e4ecf15435bb33024e076571ee3e',
+  expiresIn: '5d',
+}
