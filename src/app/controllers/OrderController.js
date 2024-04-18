0e6ab5234a00cb6d2dc0e22934fcc5a27a5ceb86
lwmoreira <lwmoreira01@gmail.com>
2024-04-18 18:02:20 -0300

    back-end-final

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup'
 import Order from '../schemas/Order'
 import Product from '../models/Product'
+import Category from '../models/Category'
+import User from '../models/User'
 
 class OrderController {
   async store(request, response) {
@@ -29,6 +31,24 @@ class OrderController {
       where: {
         id: productsIds,
       },
+      include: {
+        model: Category,
+        as: 'category',
+        attributes: ['name'],
+      },
+    })
+
+    const formattedProducts = findProducts.map((product) => {
+      const productIndex = products.findIndex((item) => item.id === product.id)
+      const newProduct = {
+        id: product.id,
+        name: product.name,
+        category: product.category.name,
+        price: product.price,
+        url: product.url,
+        quantity: products[productIndex].quantity,
+      }
+      return newProduct
     })
 
     const order = {
@@ -36,10 +56,46 @@ class OrderController {
         id: request.userId,
         name: request.userName,
       },
-      products: findProducts,
+      products: formattedProducts,
+      status: 'Pedido realizado!',
     }
 
-    return response.status(201).json(order)
+    const createOrder = await Order.create(order)
+    return response.status(201).json(createOrder)
+  }
+
+  async index(request, response) {
+    const orders = await Order.find()
+
+    return response.json(orders)
+  }
+
+  async update(request, response) {
+    const schema = Yup.object({
+      status: Yup.string().required(),
+    })
+
+    try {
+      schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      return response.status(400).json({ error: err.errors })
+    }
+
+    const { admin: isAdmin } = await User.findByPk(request.userId)
+
+    if (!isAdmin) {
+      return response.status(401).json({ error: 'Unauthorized' })
+    }
+
+    const { id } = request.params
+    const { status } = request.body
+
+    try {
+      await Order.updateOne({ _id: id }, { status })
+    } catch (err) {
+      return response.status(400).json({ error: err.message })
+    }
+    return response.json({ message: 'status updated sucessfully ' })
   }
 }
 export default new OrderController()
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,39 +1,45 @@
 import * as Yup from 'yup'
-import User from '../models/User'
 import jwt from 'jsonwebtoken'
+import User from '../models/User'
 import authConfig from '../../config/auth'
 
 class SessionController {
   async store(request, response) {
     const schema = Yup.object().shape({
       email: Yup.string().email().required(),
-      password: Yup.string().required(),
+      password: Yup.string().min(9).required(),
     })
 
-    const userEmailOrPasswordIncorrect = () => {
+    const emailOrPasswordIncorrect = () => {
       return response
         .status(401)
-        .json({ error: 'make sure your password or email are correct' })
+        .json({ error: 'Make sure your email or pa  ssword are correct' })
     }
 
     const { email, password } = request.body
 
-    if (!(await schema.isValid(request.body)))
-      return userEmailOrPasswordIncorrect()
+    if (!(await schema.isValid(request.body))) return emailOrPasswordIncorrect
 
     const user = await User.findOne({
-      where: { email },
+      where: {
+        email,
+      },
     })
 
-    if (!user) return userEmailOrPasswordIncorrect()
+    if (!user) {
+      emailOrPasswordIncorrect()
+    }
 
-    if (!(await user.checkPassword(password)))
-      return userEmailOrPasswordIncorrect()
+    const isSamePassword = await user.checkPassword(password)
 
-    return response.json({
+    if (!isSamePassword) {
+      emailOrPasswordIncorrect()
+    }
+
+    return response.status(201).json({
       id: user.id,
-      email,
       name: user.name,
+      email,
       admin: user.admin,
       token: jwt.sign({ id: user.id, name: user.name }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
@@ -43,56 +49,3 @@ class SessionController {
 }
 
 export default new SessionController()
-
-// import * as Yup from 'yup'
-// import jwt from 'jsonwebtoken'
-// import User from '../models/User'
-// import authConfig from '../../config/auth'
-
-// class SessionController {
-//   async store(request, response) {
-//     const schema = Yup.object().shape({
-//       email: Yup.string().email().required(),
-//       password: Yup.string().min(9).required(),
-//     })
-
-//     const userEmailOrPasswordIncorrect = () => {
-//       return response
-//         .status(401)
-//         .json({ error: 'Make sure your email or pa  ssword are correct' })
-//     }
-
-//     const { email, password } = request.body
-
-//     if(!(await schema.isValid(request.body)))
-//     return userEmailOrPasswordIncorrect
-
-//     const user = await User.findOne({
-//       where: {
-//         email,
-//       },
-//     })
-
-//     if (!user) {
-//       emailOrPasswordIncorrect()
-//     }
-
-//     const isSamePassword = await user.checkPassword(password)
-
-//     if (!isSamePassword) {
-//       emailOrPasswordIncorrect()
-//     }
-
-//     return response.status(201).json({
-//       id: user.id,
-//       name: user.name,
-//       email,
-//       admin: user.admin,
-//       token: jwt.sign({ id: user.id, name: user.name }, authConfig.secret, {
-//         expiresIn: authConfig.expiresIn,
-//       }),
-//     })
-//   }
-// }
-
-// export default new SessionController()
