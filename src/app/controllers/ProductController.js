56003af2c70f5b3bd326f1a45da8e8d76dedb8db
lwmoreira <lwmoreira01@gmail.com>
2024-07-29 17:49:05 -0300

    ajuste

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -5,7 +5,6 @@ import User from '../models/User.js';
 
 class ProductController {
   async store(request, response) {
-    // Definindo o esquema de validação
     const schema = Yup.object({
       name: Yup.string().required('Name is required'),
       price: Yup.number().required('Price is required').positive('Price must be positive'),
@@ -13,7 +12,6 @@ class ProductController {
       offer: Yup.boolean()
     });
 
-    // Validando os dados do corpo da requisição
     try {
       await schema.validate(request.body, { abortEarly: false });
     } catch (err) {
@@ -21,17 +19,19 @@ class ProductController {
     }
 
     // Verificando se o usuário é um administrador
-    const { admin: isAdmin } = await User.findByPk(request.userId);
-    if (!isAdmin) {
+    if (!request.userId) {
       return response.status(401).json({ error: 'Unauthorized' });
     }
 
-    // Obtendo o caminho do arquivo e os dados do produto
+    const user = await User.findByPk(request.userId);
+    if (!user || !user.admin) { // Verificação de existência do usuário
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
     const { filename: path } = request.file || {}; // Garantindo que `request.file` existe
     const { name, price, category_id, offer } = request.body;
 
     try {
-      // Criando o produto
       const product = await Product.create({
         name,
         price,
@@ -40,14 +40,13 @@ class ProductController {
         offer
       });
 
-      return response.status(201).json(product); // Código de status 201 para criação bem-sucedida
+      return response.status(201).json(product);
     } catch (error) {
       return response.status(500).json({ error: 'Internal Server Error' });
     }
   }
 
   async update(request, response) {
-    // Definindo o esquema de validação
     const schema = Yup.object({
       name: Yup.string(),
       price: Yup.number().positive('Price must be positive'),
@@ -55,7 +54,6 @@ class ProductController {
       offer: Yup.boolean()
     });
 
-    // Validando os dados do corpo da requisição
     try {
       await schema.validate(request.body, { abortEarly: false });
     } catch (err) {
@@ -63,19 +61,21 @@ class ProductController {
     }
 
     // Verificando se o usuário é um administrador
-    const { admin: isAdmin } = await User.findByPk(request.userId);
-    if (!isAdmin) {
+    if (!request.userId) {
       return response.status(401).json({ error: 'Unauthorized' });
     }
 
-    // Obtendo o ID do produto e verificando se ele existe
+    const user = await User.findByPk(request.userId);
+    if (!user || !user.admin) { // Verificação de existência do usuário
+      return response.status(401).json({ error: 'Unauthorized' });
+    }
+
     const { id } = request.params;
     const findProduct = await Product.findByPk(id);
     if (!findProduct) {
       return response.status(404).json({ error: 'Product not found' });
     }
 
-    // Atualizando o produto
     const { name, price, category_id, offer } = request.body;
     const path = request.file ? request.file.filename : findProduct.path; // Usando o caminho antigo se nenhum novo for fornecido
 
@@ -93,7 +93,6 @@ class ProductController {
 
   async index(request, response) {
     try {
-      // Obtendo todos os produtos, incluindo a categoria
       const products = await Product.findAll({
         include: [
           {
