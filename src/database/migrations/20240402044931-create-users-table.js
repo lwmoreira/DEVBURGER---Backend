adc39c25721f47c52e3a83f57221d3d3f1c83102
lwmoreira <lwmoreira01@gmail.com>
2024-09-02 16:16:37 -0300

    novo config permissão admin migration user

--- src/database/migrations/20240402044931-create-users-table.js
+++ src/database/migrations/20240402044931-create-users-table.js
@@ -29,7 +29,7 @@ module.exports = {
         type: Sequelize.BOOLEAN,
         defaultValue: true,
         allowNull: false,
-      },
+      },  
 
       createdAt: {
         type: Sequelize.DATE,
